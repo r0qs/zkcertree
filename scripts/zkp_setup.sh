0f9e454fedf6bdbd5fc300057850243932a34b5b
Rodrigo Q. Saramago <rodrigoqsaramago@gmail.com>
2022-06-08 21:31:03 +0200

    Build script

---
+++ scripts/zkp_setup.sh
@@ -0,0 +1,195 @@
+#!/bin/bash
+
+# Load helper functions
+SCRIPT_DIR="$( cd "$( dirname "$0" )" >/dev/null 2>&1 && pwd )"
+ROOT_DIR=$( dirname "$SCRIPT_DIR" )
+CONTRACTS_DIR=${ROOT_DIR}/contracts
+INPUTS_DIR=${ROOT_DIR}/private # for tests
+
+# circuits with max 2^POWERS_OF_TAU constraints
+POWERS_OF_TAU=15
+ARTIFACTS_DIR=build
+TAU_DIR=${ARTIFACTS_DIR}/ptau
+
+function init() {
+	mkdir -p ${TAU_DIR}
+
+	circuit_source=( issue approve score auth )
+	for circuit_file in "${circuit_source[@]}"; do
+		if [ ! -d "${ARTIFACTS_DIR}/${circuit_file}" ]; then
+			mkdir -p ${ARTIFACTS_DIR}/${circuit_file}
+		fi
+		if [ ! -d "${INPUTS_DIR}/${circuit_file}" ]; then
+			mkdir -p ${INPUTS_DIR}/${circuit_file}
+		fi
+	done
+}
+
+function fatal() {
+	echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&2
+	exit 1
+}
+
+function check_args() {
+	[[ "$#" -ne 2 ]] && fatal "Missing arguments: ${1}"
+}
+
+# https://docs.circom.io/getting-started/proving-circuits/#powers-of-tau
+# Generates a setup with only ONE trusted entity
+# NOTE: phase 1 can be reused in other phase 2 plonk circuits
+function phase1() {
+  # Phase1: Setup ceremony
+  # output: ptau${POWERS_OF_TAU}_0000.ptau (powers of tau ceremony initial parameters)
+  snarkjs powersoftau new bn128 12 ${TAU_DIR}/pot${POWERS_OF_TAU}_0000.ptau -v
+  snarkjs powersoftau contribute ${TAU_DIR}/pot${POWERS_OF_TAU}_0000.ptau ${TAU_DIR}/pot${POWERS_OF_TAU}_0001.ptau --name="1st contribution" -v -e="$(head -n 4096 /dev/urandom | openssl sha256)"
+
+  # Verify Phase1:
+  # snarkjs powersoftau verify ${TAU_DIR}/pot${POWERS_OF_TAU}_0001.ptau
+
+  # Apply random beacon to finalised this phase of the setup.
+  # For more information about random beacons see here: https://eprint.iacr.org/2017/1050.pdf
+  # From: https://github.com/iden3/snarkjs/blob/master/README.md#6-apply-a-random-beacon
+  # In this example the beacon is essentially a delayed hash function evaluated on 0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f
+  # (in practice this value will be some form of high entropy and publicly available data of your choice)
+  snarkjs powersoftau beacon ${TAU_DIR}/pot${POWERS_OF_TAU}_0001.ptau ${TAU_DIR}/pot${POWERS_OF_TAU}_beacon.ptau 0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f 10 -n="Final Beacon"
+
+  preparePhase2
+}
+
+function preparePhase2() {
+  # Prepare Phase2: Circuit specific ceremony and proving key generation
+  # output: pot${POWERS_OF_TAU}_final.ptau (after all phase1 contritutions, none in this case)
+  snarkjs powersoftau prepare phase2 ${TAU_DIR}/pot${POWERS_OF_TAU}_beacon.ptau ${TAU_DIR}/pot${POWERS_OF_TAU}.ptau -v
+
+  # Verify the final ptau file
+  snarkjs powersoftau verify ${TAU_DIR}/pot${POWERS_OF_TAU}.ptau
+}
+
+function downloadTAU() {
+  if [ ! -f ${TAU_DIR}/pot${POWERS_OF_TAU}.ptau ]; then
+    echo "Downloading powers of tau file"
+    curl -L https://hermez.s3-eu-west-1.amazonaws.com/powersOfTau28_hez_final_$POWERS_OF_TAU.ptau --create-dirs -o ${TAU_DIR}/pot${POWERS_OF_TAU}.ptau
+  fi
+}
+
+# https://docs.circom.io/getting-started/proving-circuits/#powers-of-tau
+# Generates a setup with only ONE trusted entity
+function phase2() {
+	check_args $1 "circuit not informed or found"
+	circuit=$1
+	# output: ${circuit}.zkey file that will contain the proving and verification keys together with all phase 2 contributions
+	snarkjs plonk setup ${ARTIFACTS_DIR}/${circuit}/${circuit}.r1cs ${TAU_DIR}/pot${POWERS_OF_TAU}.ptau ./build/${circuit}/${circuit}.zkey -v
+
+	# groth16 require setup for each circuit
+	# snarkjs groth16 setup ./build/${circuit}/${circuit}.r1cs ./build/pot${POWERS_OF_TAU}_final.ptau ./build/${circuit}/${circuit}_0000.zkey
+
+	# the ${circuit}_X.zkey would be the final circuit after X contributors
+	# snarkjs zkey contribute ./build/${circuit}/${circuit}_0000.zkey ./build/${circuit}/${circuit}.zkey --name="1st Contributor" -v -e="$(head -n 4096 /dev/urandom | openssl sha256)"
+}
+
+# Compile the circuit
+# output: circuit.sym, circuit.wasm and circuit.r1cs
+function compile_circuit() {
+	check_args $1 "circuit name not informed"
+	circuit=$1
+
+	CIRCUIT_DIR=${ARTIFACTS_DIR}/${circuit}
+
+	# using circom 2.0
+	# TODO: check if circom is installed
+	circom circuits/${circuit}.circom --r1cs --wasm --sym -o ${ARTIFACTS_DIR}/${circuit}
+	if [ -d "./build/${circuit}/${circuit}_js" ]; then
+		mv ./build/${circuit}/${circuit}_js/* ./build/${circuit}
+		rm -rf ./build/${circuit}/${circuit}_js
+	fi
+}
+
+function export_verification_key() {
+	check_args $1 "circuit name not informed"
+	circuit=$1
+
+	CIRCUIT_DIR=${ARTIFACTS_DIR}/${circuit}
+
+	# export verification key
+	snarkjs zkey export verificationkey ${CIRCUIT_DIR}/${circuit}.zkey ${CIRCUIT_DIR}/verification_key.json
+}
+
+function gen_verifier_contract() {
+	check_args $1 "circuit name not informed"
+	circuit=$1
+
+	CIRCUIT_DIR=${ARTIFACTS_DIR}/${circuit}
+
+	# generate the Verifier smart contract
+	snarkjs zkey export solidityverifier ${CIRCUIT_DIR}/${circuit}.zkey ${CONTRACTS_DIR}/Verifier_${circuit}.sol -v
+
+	# output the contract's call parameters (to be passed to the verify function)
+	# snarkjs zkey export soliditycalldata ./build/public.json ./build/proof.json | tee parameters.txt
+}
+
+function gen_proof() {
+	check_args $1 "circuit name not informed"
+	circuit=$1
+
+	CIRCUIT_DIR=${ARTIFACTS_DIR}/${circuit}
+	INPUTS=${INPUTS_DIR}/${circuit}
+	# echo "{ \"in\": [1,2,3,4], \"result\": 2 }" > priv/inputs.json
+
+	# calculate witness by running the circuit over the inputs
+	# output: witness.wtns (private witness binary)
+	# snarkjs wtns calculate ${CIRCUIT_DIR}/${circuit}.wasm ${INPUTS}/inputs.json ${INPUTS}/witness.wtns
+
+	node ${CIRCUIT_DIR}/generate_witness.js ${CIRCUIT_DIR}/${circuit}.wasm ${INPUTS}/inputs.json ${INPUTS}/witness.wtns
+
+	# export witness
+	# output: witness.json
+	snarkjs wtns export json ${INPUTS}/witness.wtns ${INPUTS}/witness.json
+
+	# generate the proof using the proving zkey and witness
+	# output: proof.json and public.json
+	snarkjs plonk prove ${CIRCUIT_DIR}/${circuit}.zkey ${INPUTS}/witness.wtns ${INPUTS}/proof.json ${INPUTS}/public.json
+
+	# generate the proof using the proving zkey and private inputs
+	# snarkjs plonk fullprove ${INPUTS}/inputs.json ${CIRCUIT_DIR}/${circuit}.wasm ./build/circuit_${circuit}.zkey ${INPUTS}/proof.json ${INPUTS}/public.json
+
+	# snarkjs groth16 prove ${CIRCUIT_DIR}/${circuit}.zkey ${INPUTS}/witness.wtns ${INPUTS}/proof.json ${INPUTS}/public.json
+}
+
+function verify() {
+	check_args $1 "circuit name not informed"
+	circuit=$1
+
+	CIRCUIT_DIR=${ARTIFACTS_DIR}/${circuit}
+	INPUTS=${INPUTS_DIR}/${circuit}
+
+	# verify proof
+	snarkjs plonk verify ${CIRCUIT_DIR}/verification_key.json ${INPUTS}/public.json ${INPUTS}/proof.json
+}
+
+usage() {
+	echo "usage: ${0} [option]"
+	echo 'options:'
+	echo '    -download-tau  Download powers of tau'
+	echo '    -phase1  Run ZKP Setup Phase1 and prepare to Phase2'
+	echo '    -phase2  Run ZKP Setup Phase2'
+	echo '    -compile  Compile circuits'
+	echo '    -export-vkey  Export verification key json'
+	echo '    -gen-vcontract  Generate verifier smart contract'
+	echo '    -gen-proof  Generate Plonk proof'
+	echo '    -verify  Verify proof'
+	echo
+}
+
+init
+option="${1}"
+case ${option} in
+	-download-tau) downloadTAU;;
+  -phase1) phase1;;
+	-phase2) phase2 "${@:2}";;
+	-compile) compile_circuit "${@:2}";;
+	-export-vkey) export_verification_key "${@:2}";;
+	-gen-vcontract) gen_verifier_contract "${@:2}";;
+	-gen-proof) gen_proof "${@:2}";;
+	-verify) verify "${@:2}";;
+	*) usage; exit 1;;
+esac
