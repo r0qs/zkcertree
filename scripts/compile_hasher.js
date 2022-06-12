b16ea57f16f5061d41ec2b5b7035d7422b3803c8
Rodrigo Q. Saramago <rodrigoqsaramago@gmail.com>
2022-06-12 23:34:33 +0200

    Add compiler hasher script

---
+++ scripts/compile_hasher.js
@@ -0,0 +1,24 @@
+// Modified from: https://github.com/tornadocash/tornado-nova/blob/master/scripts/compileHasher.js
+
+// Generates Hasher artifact at compile-time
+const path = require('path')
+const fs = require('fs')
+const {createCode, generateABI} = require('circomlibjs').poseidonContract;
+const outputPath = path.join(__dirname, '..', 'build', 'contracts', 'Hasher.sol')
+const outputFile = path.join(outputPath, 'Hasher.json')
+
+if (!fs.existsSync(outputPath)) {
+  fs.mkdirSync(outputPath, { recursive: true })
+}
+
+const contract = {
+  _format: 'hh-sol-artifact-1',
+  sourceName: 'contracts/Hasher.sol',
+  linkReferences: {},
+  deployedLinkReferences: {},
+  contractName: 'Hasher',
+  abi: generateABI(2),
+  bytecode: createCode(2),
+}
+
+fs.writeFileSync(outputFile, JSON.stringify(contract, null, 2))
